2a97915b57fbecbc98023f4fec16344276c4da9e
firasqais <30744313+firasqais@users.noreply.github.com>
2023-11-17 12:26:17 +1100

    Initial Commit

---
+++ myown/myownApp.swift
@@ -0,0 +1,17 @@
+//
+//  myownApp.swift
+//  myown
+//
+//  Created by Firas on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct myownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
